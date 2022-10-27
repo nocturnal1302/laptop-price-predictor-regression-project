023518a7abfcce31dae8f2cd1e0f3b75859e3303
Trinh Tu <tutrinh1302.memes@gmail.com>
2022-10-28 01:59:19 +0700

    updated web app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
+st.title("Laptop Price Predictor")
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
@@ -15,10 +15,10 @@ company = st.selectbox('Brand',df['Company'].unique())
 type = st.selectbox('Type',df['TypeName'].unique())
 
 # Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+ram = st.selectbox('RAM (in GB)',[2,4,6,8,12,16,24,32,64])
 
 # weight
-weight = st.number_input('Weight of the Laptop')
+weight = st.number_input('Weight of the Laptop (in kg)')
 
 # Touchscreen
 touchscreen = st.selectbox('Touchscreen',['No','Yes'])
@@ -27,7 +27,7 @@ touchscreen = st.selectbox('Touchscreen',['No','Yes'])
 ips = st.selectbox('IPS',['No','Yes'])
 
 # screen size
-screen_size = st.number_input('Screen Size')
+screen_size = st.number_input('Screen Size (in inches)')
 
 # resolution
 resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
@@ -35,9 +35,9 @@ resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900'
 #cpu
 cpu = st.selectbox('CPU',df['Cpu brand'].unique())
 
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+hdd = st.selectbox('HDD (in GB)',[0,128,256,512,1024,2048])
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+ssd = st.selectbox('SSD (in GB)',[0,8,128,256,512,1024])
 
 gpu = st.selectbox('GPU',df['Gpu brand'].unique())
 
@@ -62,5 +62,5 @@ if st.button('Predict Price'):
     query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
     query = query.reshape(1,12)
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))) + " VND.")
 
